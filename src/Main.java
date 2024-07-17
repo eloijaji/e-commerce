96dc0d3a1855234be5189b0d6ebfe4e2d75ac5cb
asmaa <eloijaji@et.esiea.fr>
2024-07-17 16:27:14 -0400

    Message de commit pour la fusion

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
 public class Main {
-    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User("Asma EL OIJAJI", "asma.eloijaji@example.com");
+        user.register();
+        user.login();
+
+        Product product1 = new Product("Makeup Brush", 1000, 1);
+        Product product2 = new Product("Black Dress", 50, 2);
+
+        Card card = new Card(user);
+        card.addProduct(product1);
+        card.addProduct(product2);
+
+        card.checkout();
+
+        Order order = new Order("12345");
+        OrderObserver priceObserver = new PriceObserver();
+        OrderObserver quantityObserver = new QuantityObserver();
+
+        order.attach(priceObserver);
+        order.attach(quantityObserver);
+
+
+        order.addItem(500);
+        order.addItem(150);
+
+        order.detach(priceObserver);
+
+        order.addItem(200);
     }
 }
